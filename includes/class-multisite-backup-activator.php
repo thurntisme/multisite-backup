18567d269a95eaa73e219caf7e1be2d60082abc7
Sotatek-ThuyNguyen4 <thuy.nguyen4@sotatek.com>
2025-12-11 09:51:19 +0700

    Initial commit

---
+++ includes/class-multisite-backup-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://thurntisme.com
+ * @since      1.0.0
+ *
+ * @package    Multisite_Backup
+ * @subpackage Multisite_Backup/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Multisite_Backup
+ * @subpackage Multisite_Backup/includes
+ * @author     Thủy Nguyễn Thế <nguyenthethuy.qnam@gmail.com>
+ */
+class Multisite_Backup_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-multisite-backup-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://thurntisme.com
+ * @since      1.0.0
+ *
+ * @package    Multisite_Backup
+ * @subpackage Multisite_Backup/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Multisite_Backup
+ * @subpackage Multisite_Backup/includes
+ * @author     Thủy Nguyễn Thế <nguyenthethuy.qnam@gmail.com>
+ */
+class Multisite_Backup_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
